16bda0733276ff10771583d8cf648c5cd309c960
wldnd9895 <wldnd9895@naver.com>
2026-05-04 17:14:00 +0900

    1장 데이터베이스 실습

--- 1장_데이터베이스_개요_및_설치.sql
+++ 1장_데이터베이스_개요_및_설치.sql
@@ -9,4 +9,24 @@
 
 # 데이터베이스 생성, 제거
 CREATE DATABASE mydb;
-DROP DATABASE mydb;
+DROP DATABASE mydb;		-- 실행 단축키 : ctrl + enter
+
+# 데이터베이스 목록 조회
+SHOW DATABASES;
+
+# 작업 데이터베이스 선택
+USE mydb;
+USE studydb;
+
+---------------------------------------------------
+# 일반 관리자 생성, 권한 부여, 반영
+CREATE USER 'admin'@'%' IDENTIFIED BY '1234'; -- 유저 생성, %는 외부접속 IP를 의미
+GRANT ALL PRIVILEGES ON MYDB.* TO 'admin'@'%'; -- admin 에게 mydb의 모든권한(CRUD) 부여
+FLUSH PRIVILEGES; -- admin 계정권한 반영
+
+# 비밀번호 변경
+ALTER USER 'admin'@'%' IDENTIFIED BY 'abcd';
+
+# 계정 삭제
+DROP USER 'admin'@'%';
+
