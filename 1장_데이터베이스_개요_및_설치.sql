e2f7abe04d202812c046d7ff803eb0bc58ddbbdb
wldnd9895 <wldnd9895@naver.com>
2026-05-04 16:13:21 +0900

    데이터베이스 1장 실습

---
+++ 1장_데이터베이스_개요_및_설치.sql
@@ -0,0 +1,12 @@
+-- 날짜 : 2026/05/04
+-- 이름 : 양지웅
+-- 내용 : 1장 데이터베이스 개요 및 설치
+
+# MySQL 접속 (콘솔환경)
+#> mysql -u root -p;
+#> -u: user, 사용자 계정
+#> -p: password, 비밀번호
+
+# 데이터베이스 생성, 제거
+CREATE DATABASE mydb;
+DROP DATABASE mydb;
